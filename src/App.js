3cbb8ef6e9f72891b5ea96d447447f58817b181e
tektechnologies <info@tektechnologies.com>
2023-02-24 11:00:38 -0600

    update nav add navlink

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 //1. Import our other components
-import AboutUs from './pages/AboutUs';
-import OtherContent from './pages/FAQ';
+import AboutUs from "./pages/AboutUs";
+import FAQ from "./pages/FAQ";
 import NavHeader from "./components/NavHeader";
+import Home from './pages/Home';
 //Docs = https://reactrouter.com/en/main/start/overview
 //2. Step one add npm i react-router-dom@6.4.4
 //https://www.npmjs.com/package/react-router-dom/v/6.4.4
 
-//3. Add the import for our router. 
+//3. Add the import for our router.
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -16,17 +17,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-
-//3. add our create Browser router 
+//3. add our create Browser router
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<NavHeader />}>
-      <Route path="aboutus" element={<AboutUs />} />
-      <Route path="othercontent" element={<OtherContent />} />
+      <Route index element={<Home />} />
+      <Route path="about" element={<AboutUs />} />
+      <Route path="faq" element={<FAQ />} />
     </Route>
   )
 );
-
 
 class App extends React.Component {
   render() {
--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
  class NavHeader extends React.Component {
   render() {
     return (
-      <div>NavHeader</div>
+      <>
+      <section className='root-layout'>
+      <header>
+        <nav>
+          <h1>React Router</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About Us</NavLink>
+          <NavLink to="faq">Faq</NavLink>
+
+        </nav>
+      </header>
+
+      </section>
+
+      </>
+      
     )
   }
 }
