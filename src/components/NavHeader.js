ff133fe4d14205bdf8104f06dcad379020d3044b
tektechnologies <info@tektechnologies.com>
2023-02-24 11:03:16 -0600

    add outlet for router

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
- class NavHeader extends React.Component {
+class NavHeader extends React.Component {
   render() {
     return (
       <>
-      <section className='root-layout'>
-      <header>
-        <nav>
-          <h1>React Router</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About Us</NavLink>
-          <NavLink to="faq">Faq</NavLink>
-
-        </nav>
-      </header>
-
-      </section>
-
+        <section className="root-layout">
+          <header>
+            <nav>
+              <h1>React Router</h1>
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="about">About Us</NavLink>
+              <NavLink to="faq">Faq</NavLink>
+            </nav>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </section>
       </>
-      
-    )
+    );
   }
 }
 
