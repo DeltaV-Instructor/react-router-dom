7fd100e4bc60e45d25cb18bf5dfbad39b5aea326
tektechnologies <info@tektechnologies.com>
2023-02-24 15:23:03 -0600

    add axios mock call to server

--- src/pages/SecretContent.js
+++ src/pages/SecretContent.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react'
-import { withAuth0 } from '@auth0/auth0-react';
-
-
- class SecretContent extends Component {
-  constructor(props){
+import React, { Component } from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
+class SecretContent extends Component {
+  constructor(props) {
     super(props);
-    this.state ={
-      treasure: []
+    this.state = {
+      treasure: [],
+    };
+  }
+
+  async componentDidMount() {
+    if (this.props.auth0.isAuthenticated) {
+      const res = await this.props.auth0.getIdTokenClaims();
+      const jwt = res.__raw;
+      console.log("token: ", jwt);
+
+      const config = {
+        headers: { Authorization: `Bearer ${jwt}` },
+        method: "get",
+        baseURL: process.env.REACT_APP_SERVER,
+        url: "/our-route",
+      };
+      //build URL
+      let axiosData = await axios(config);
+
+      let treasureData = ["gold", "gems", "pizza"];
     }
+
+    this.setState({
+      treasure: treasureData,
+    });
   }
   render() {
-    return (
-      <h1>SecretContent we hope</h1>
-
-    )
+    console.log(this.state.treasure);
+    return <h1>SecretContent we hope</h1>;
   }
 }
 
-
-export default SecretContent;
+export default withAuth0(SecretContent);
