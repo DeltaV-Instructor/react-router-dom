8e5306f0ecac77427c525a47ed1a5aeccb20c39e
tektechnologies <info@tektechnologies.com>
2023-02-24 11:23:47 -0600

    add content to site with pic of cat

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>Lets add some router Navigation</h1>
         <RouterProvider router={router} />
       </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react';
-
- class Home extends React.Component {
+import React from "react";
+import cat from "../images/cat.jpg";
+class Home extends React.Component {
   render() {
     return (
-      <div>WELCOME HOME</div>
-    )
+      <>
+        <div>WELCOME HOME</div>
+        <img src={cat} alt={'cat'}/>
+      </>
+    );
   }
 }
 
