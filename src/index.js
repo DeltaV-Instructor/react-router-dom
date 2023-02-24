6640a4e520bd0978e20ae85a0d4cf5393f9e7213
tektechnologies <info@tektechnologies.com>
2023-02-24 14:14:15 -0600

    add auth0 provider

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+  domain="dev-818my7zr.us.auth0.com"
+  clientId="1SNr2B95r1JtZwK6dUKNzqCSOc21ccIp"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  <App />
+</Auth0Provider>
 );
 
 
