bdcefce3999d02932c4923ebfb90d7e7bb6f2982
tektechnologies <info@tektechnologies.com>
2023-02-24 14:51:40 -0600

    add profile

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import HelpComponent from "./components/HelpComponent";
 //https://www.npmjs.com/package/react-router-dom/v/6.4.4
 import Login from './components/Login';
 import Logout from './components/Logout';
+import Profile from './components/Profile';
 
 //3. Add the import for our router.
 import {
@@ -31,6 +32,7 @@ const router = createBrowserRouter(
 
       <Route path='login' element={<Login />} />
       <Route path='logout' element={<Logout />} />
+      <Route path='profile' element={<Profile />} />
       
       <Route path="help" element={<HelpComponent />}>
         <Route path="faq" element={<FAQ /> } />
